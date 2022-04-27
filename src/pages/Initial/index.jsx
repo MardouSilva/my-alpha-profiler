6d89dd1f32542c79a74d090038924bb32ec74b55
Marcos Douglas <mardousilva@gmail.com>
2022-04-27 19:44:47 -0300

    pastas de componentes adicionadas

---
+++ src/pages/Initial/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.css"
+
+export const Initial = () => {
+  return <h1 id="initialTitle">Initial</h1>;
+};
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import { Profile } from "../pages/EditProfile"
 import { Home } from "../pages/Home"
+import { Initial } from "../pages/Initial";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Initial />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/editprofile" element={<Profile />} />
