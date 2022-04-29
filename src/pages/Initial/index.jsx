a170d842358a60783c82aedfbfaec5fb1dfd29cd
Marcos Douglas <mardousilva@gmail.com>
2022-04-29 16:24:54 -0300

    configuração inicial das pages

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 import "./styles.css"
 
 export const Initial = () => {
   return( 
     <div id="initialTitle">
-      
+      <p id="initialDescription">
+        Clube destinado as pessoas que compreendem a verdade 
+        e não acreditam na mentira contada pelos governos
+        mundiais 
+      </p>
+      <button id="initialButtonColored">Começar</button>
     </div> 
   )
 };
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,8 +2,13 @@ import "./styles.css"
 
 export const SignIn = () => {
   return (
-    <div id="signinTitle">
-      
-    </div>
+    <section id="signinScreen">
+      <div id="signinContainer">
+        <h1>Logar</h1>
+        <input type="text" placeholder="Usuário" />
+        <input type="password" placeholder="Senha" />
+        <button id="signinButtonColored">Logar</button>
+      </div>
+    </section>
   ) 
 };
