fdfffc6f8f6168253a789ffe67096a9afc4a7e56
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-05-01 13:54:21 -0400

    Tela cadastro

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,16 +1,10 @@
-import { useNavigate } from "react-router-dom";
-
-import "./styles.css"
+import "./styles.css";
 
 export const Initial = () => {
-  return( 
+  return (
     <div id="initialTitle">
-      <p id="initialDescription">
-        Clube destinado as pessoas que compreendem a verdade 
-        e não acreditam na mentira contada pelos governos
-        mundiais 
-      </p>
+      <p id="initialDescription">Clube destinado as pessoas que compreendem a verdade e não acreditam na mentira contada pelos governos mundiais</p>
       <button id="initialButtonColored">Começar</button>
-    </div> 
-  )
+    </div>
+  );
 };
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,16 @@
-import "./styles.css"
+import "./styles.css";
 
 export const SignUp = () => {
   return (
-    <div id="signupTitle">SignUp</div>
-  )  
+    <section id="signupScreen">
+      <div id="signupContainer">
+        <h1>Cadastrar</h1>
+        <input type="text" placeholder="Usuário" />
+        <input type="password" placeholder="Senha" />
+        <input type="email" placeholder="E-mail" />
+        <input type="date" placeholder="Data de Nascimento" />
+        <button id="signupButtonColored">Cadastrar</button>
+      </div>
+    </section>
+  );
 };
