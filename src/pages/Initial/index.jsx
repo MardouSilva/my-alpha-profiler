603b19c93a7e8b22cae9fb86904cfce00262a4c3
Marcos Douglas <mardousilva@gmail.com>
2022-05-02 16:30:47 -0300

    qualquer coisa

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,10 +1,16 @@
+import { Link, useNavigate } from "react-router-dom";
+
 import "./styles.css";
 
 export const Initial = () => {
+  let navigate = useNavigate()
+  const toLogin = () => {
+    navigate('/signin')
+  }
   return (
     <div id="initialTitle">
       <p id="initialDescription">Clube destinado as pessoas que compreendem a verdade e não acreditam na mentira contada pelos governos mundiais</p>
-      <button id="initialButtonColored">Começar</button>
+      <Link to="signin" ><button id="initialButtonColored">Começar</button></Link>
     </div>
   );
 };
