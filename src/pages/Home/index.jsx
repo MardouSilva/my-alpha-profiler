f3d00d896fd2f044043766f913cde33b636bb2d7
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-05-06 22:10:22 -0400

    exemplo sem jsx na HOME

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export const Home = () => {
@@ -16,42 +16,70 @@ export const Home = () => {
     setUserInfo(responseJson);
     console.log(userInfo);
   }
-  
+
   const getAge = () => {
     const now = new Date(); // Data de hoje
     const past = new Date(userInfo.birth_date); // Outra data no passado
     const diff = Math.abs(now.getTime() - past.getTime()); // Subtrai uma data pela outra
     return Math.ceil(diff / (1000 * 60 * 60 * 24 * 30 * 12));
-  }
-  useEffect(()=>{
+  };
+  useEffect(() => {
     getUserInfo();
-  })
+  });
 
-  return (
-    <section id="homeScreen">
-      <div id="infoContainer">
-        <div id="blueCardsContainer">
-          <div class="blueCards">
-            <p class="cardTitle">Nome</p>
-            <p class="blueCardTextContent">{userInfo.username}</p>
-          </div>
-          <div class="blueCards">
-            <p class="cardTitle">Idade</p>
-            <p class="blueCardTextContent">{getAge()}</p>
-          </div>
-        </div>
-        <div id="violetCardsContainer">
-          <div class="violetCards">
-            <p class="cardTitle">Nascimento</p>
-            <p class="violetCardTextContent">{userInfo.birth_date}</p>
-          </div>
-          <div class="violetCards">
-            <p class="cardTitle">Email</p>
-            <p class="violetCardTextContent">{userInfo.email}</p>
-          </div>
-        </div>
-        <img class="imageUser" src="https://i.pinimg.com/564x/8b/da/ca/8bdaca81d5ddbaeb92b61d6b5787d866.jpg" alt="UserPerfil"></img>
-      </div>
-    </section>
+  return React.createElement(
+    "section",
+    { id: "homeScreen" },
+    React.createElement("div", { id: "infoContainer" }, [
+      React.createElement("div", { id: "blueCardsContainer" }, [
+        React.createElement("div", { className: "blueCards" }, [
+          React.createElement("p", { className: "cardTitle" }, "Nome"),
+          React.createElement("p", { className: "blueCardTextContent" }, userInfo.username),
+        ]),
+        React.createElement("div", { className: "blueCards" }, [
+          React.createElement("p", { className: "cardTitle" }, "Idade"),
+          React.createElement("p", { className: "blueCardTextContent" }, getAge()),
+        ]),
+      ]),
+      React.createElement("div", { id: "violetCardsContainer" }, [
+        React.createElement("div", { className: "violetCards" }, [
+          React.createElement("p", { className: "cardTitle" }, "Nome"),
+          React.createElement("p", { className: "violetCardTextContent" }, userInfo.birth_date),
+        ]),
+        React.createElement("div", { className: "violetCards" }, [
+          React.createElement("p", { className: "cardTitle" }, "Idade"),
+          React.createElement("p", { className: "violetCardTextContent" }, userInfo.email),
+        ]),
+      ]),
+      React.createElement("img", { className: "imageUser", src: "https://i.pinimg.com/564x/8b/da/ca/8bdaca81d5ddbaeb92b61d6b5787d866.jpg", alt: "UserPerfil" }),
+    ])
   );
+
+  // return (
+  //   <section id="homeScreen">
+  //     <div id="infoContainer">
+  //       <div id="blueCardsContainer">
+  //         <div class="blueCards">
+  //           <p class="cardTitle">Nome</p>
+  //           <p class="blueCardTextContent">{userInfo.username}</p>
+  //         </div>
+  //         <div class="blueCards">
+  //           <p class="cardTitle">Idade</p>
+  //           <p class="blueCardTextContent">{getAge()}</p>
+  //         </div>
+  //       </div>
+  //       <div id="violetCardsContainer">
+  //         <div class="violetCards">
+  //           <p class="cardTitle">Nascimento</p>
+  //           <p class="violetCardTextContent">{userInfo.birth_date}</p>
+  //         </div>
+  //         <div class="violetCards">
+  //           <p class="cardTitle">Email</p>
+  //           <p class="violetCardTextContent">{userInfo.email}</p>
+  //         </div>
+  //       </div>
+  //       <img class="imageUser" src="https://i.pinimg.com/564x/8b/da/ca/8bdaca81d5ddbaeb92b61d6b5787d866.jpg" alt="UserPerfil"></img>
+  //     </div>
+  //   </section>
+  // );
 };
